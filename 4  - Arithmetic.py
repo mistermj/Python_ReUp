16728e017abe1ee614687be2f4f7e05897e17ed5
mistermj <nhawk1100@gmail.com>
2018-11-03 23:25:41 -0700

    Arithmetics

---
+++ 4  - Arithmetic.py
@@ -0,0 +1,12 @@
+'''
+PEMDAS
+'''
+
+print(f' 7 to the power of 2 is : {7**2}') # Power
+print(f' 7 divided by 2 and floored is : {7//2}') # Floor
+
+# using the math library
+
+import math
+print(math.ceil(11/2))
+print(math.floor(11/2))
