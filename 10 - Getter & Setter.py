db2d3ff54e7242b55b83b093791bf371c31474e8
mistermj <nhawk1100@gmail.com>
2018-11-14 18:47:28 +0500

    final touch

--- 10 - Getter & Setter.py
+++ 10 - Getter & Setter.py
@@ -16,7 +16,7 @@ class Information:
         self._name = value + "someone changed the value"
 
     def get_intro(self):
-        return "information_name is {}".format(self.get_name)
+        return "information_name is {}".format(self.get_name())
 
 handle = Information()
 handle.set_name("meindak")
@@ -30,3 +30,29 @@ when we set or get some information, we are essentially invoking a method call.
 we are not assigning values to properties. we need to make these into properties
 '''
 
+class New:
+
+    _name = ""
+    _age = 0
+
+    def __init(self, name, age):
+        self._name = name
+        self._age = age
+
+    #getter
+    @property
+    def get_name(self):
+        return self._name.upper()
+    
+    # setter
+    @get_name.setter
+    def set_name(self, value):
+        self._name = value + " - someone changed the value"
+    #we are not invoking method
+    def get_intro(self):
+        return "information_name is {}".format(self.get_name)
+
+handle1 = New()
+handle1.set_name = "dadoo" # notice we are not calling method
+
+print(handle1.get_intro())
